7a4cc80774fce848ae112af10639cbc27e5f7af2
andrianarivo <andrianarivo.david@gmail.com>
2023-09-11 16:08:43 +0300

    Adding total_expense to Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,14 @@ class Restaurant < ApplicationRecord
   has_and_belongs_to_many :expenses
 
   validates :name, presence: true
+
+  before_save :update_total_expense
+
+  private
+
+  def update_total_expense
+    self.total_expense = expenses.reduce(0.0) do |acc, item|
+      acc + item.amount
+    end
+  end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
   subject do
-    user = User.first
-    user ||= User.create(name: 'David')
-    Restaurant.new(name: 'McDonald', user:)
+    @user = User.first
+    @user ||= User.create(name: 'David')
+    Restaurant.new(name: 'McDonald', user: @user)
   end
 
   before { subject.save }
@@ -25,5 +25,16 @@ RSpec.describe Restaurant, type: :model do
 
   it 'associated user is valid' do
     expect(subject.user).to be_valid
+  end
+
+  it 'total_spec is correct' do
+    expenses = [
+      Expense.create(name: 'exp1', amount: 1, author: @user),
+      Expense.create(name: 'exp2', amount: 2, author: @user),
+      Expense.create(name: 'exp3', amount: 3, author: @user),
+      Expense.create(name: 'exp4', amount: 4, author: @user)
+    ]
+    subject.expenses << expenses
+    expect { subject.save }.to(change { subject.total_expense })
   end
 end
