33a50ee8c1dba9a03d3b0b15539111f537fd4a66
andrianarivo <andrianarivo.david@gmail.com>
2023-09-15 11:12:01 +0300

    Update mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer
+  config.action_mailer.default_url_options = { host: "https://restaurant-budget-testing.onrender.com" }
 end
